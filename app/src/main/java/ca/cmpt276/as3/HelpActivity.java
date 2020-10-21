c1996b61fdbdb24f14f568c898761ab57b8bf2e0
zetix <zetix@sfu.ca>
2020-10-21 14:45:29 +0800

    Set the help page

--- app/src/main/java/ca/cmpt276/as3/HelpActivity.java
+++ app/src/main/java/ca/cmpt276/as3/HelpActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -16,5 +19,11 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        TextView textView = findViewById(R.id.help_page_hyperlink);
+        textView.setClickable(true);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        String text = "<a href='https://opencoursehub.cs.sfu.ca/bfraser/grav-cms/cmpt276/home'> Course Website </a>";
+        textView.setText(Html.fromHtml(text));
     }
 }
