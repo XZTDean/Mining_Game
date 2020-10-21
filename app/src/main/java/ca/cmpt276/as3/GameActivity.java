6f230453b83dd5cf0f8a998fd67bedd1cf081e47
zetix <zetix@sfu.ca>
2020-10-21 11:17:53 +0800

    Implement Game activity
    Build game activity, add needed icons, add Floating action button

--- app/src/main/java/ca/cmpt276/as3/GameActivity.java
+++ app/src/main/java/ca/cmpt276/as3/GameActivity.java
@@ -1,12 +1,29 @@
 package ca.cmpt276.as3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import ca.cmpt276.as3.model.GameBoard;
+import ca.cmpt276.as3.model.GameConfig;
+
+import static java.security.AccessController.getContext;
 
 public class GameActivity extends AppCompatActivity {
+    private GameBoard game;
+    private FloatingActionButton[][] buttons;
 
     public static Intent makeIntent(Context c) {
         return new Intent(c, GameActivity.class);
@@ -16,5 +33,144 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        game = new GameBoard();
+        populateTable();
+
+        TextView textView = findViewById(R.id.mineTotal);
+        String totalMine = String.valueOf(game.getMineNumber());
+        textView.setText(totalMine);
+    }
+
+    private void populateTable() {
+        TableLayout table = findViewById(R.id.buttonTable);
+        GameConfig config = GameConfig.getInstance();
+        buttons = new FloatingActionButton[config.getHeight()][config.getWidth()];
+        int color = ContextCompat.getColor(this, R.color.potential);
+        for (int row = 0; row < config.getHeight(); row++) {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1));
+            table.addView(tableRow);
+            final int FINAL_ROW = row;
+
+            for (int col = 0; col < config.getWidth(); col++) {
+                final int FINAL_COL = col;
+                FloatingActionButton button = new FloatingActionButton(this);
+                TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1);
+                layoutParams.setMargins(8,8,8,8);
+                button.setLayoutParams(layoutParams);
+
+                button.setImageResource(R.drawable.ic_user);
+                button.setBackgroundTintList(ColorStateList.valueOf(color));
+                button.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        scan(FINAL_COL, FINAL_ROW);
+                    }
+                });
+                buttons[row][col] = button;
+                tableRow.addView(button);
+            }
+        }
+    }
+
+    private void scan(int col, int row) {
+        switch (game.scan(col, row)) {
+            case -1: // already scan
+                break;
+            case 0: // new scan
+                setNumber(col, row, true);
+                scanRowCol(col, row);
+                setScanNum();
+                break;
+            case 1: // new mine
+                setMine(col, row);
+                reSetRelevantNumber(col, row);
+                setFoundNumber();
+                if (game.isWin()) {
+                    win();
+                }
+                break;
+            case 2: // mine with scan
+                setNumber(col, row, false);
+                scanRowCol(col, row);
+                setScanNum();
+        }
+    }
+
+    private void setFoundNumber() {
+        TextView textView = findViewById(R.id.mineFound);
+        String found = String.valueOf(game.getMineFound());
+        textView.setText(found);
+    }
+
+    private void setScanNum() {
+        TextView textView = findViewById(R.id.scanNum);
+        String scanUsed = String.valueOf(game.getScanUsed());
+        textView.setText(scanUsed);
+    }
+
+    private void setMine(int col, int row) {
+        FloatingActionButton button = buttons[row][col];
+        int color = ContextCompat.getColor(this, R.color.confirmed);
+        button.setBackgroundTintList(ColorStateList.valueOf(color));
+        button.setImageResource(R.drawable.ic_bacteria);
+    }
+
+    private void reSetRelevantNumber(int col, int row) {
+        for (int i = 0; i < game.getWidth(); i++) {
+            if (!game.cellIsHidden(i, row)) {
+                setNumber(i, row, false);
+            }
+        }
+        for (int i = 0; i < game.getHeight(); i++) {
+            if (!game.cellIsHidden(col, i)) {
+                setNumber(col, i, false);
+            }
+        }
+    }
+
+    private void setNumber(int col, int row, boolean changeColor) {
+        FloatingActionButton button = buttons[row][col];
+        if (changeColor) {
+            int color = ContextCompat.getColor(this, R.color.safe);
+            button.setBackgroundTintList(ColorStateList.valueOf(color));
+        }
+        int num = game.getMineRelevantForCell(col, row);
+        String resourceName = "ic_";
+        if (num < 10) {
+            resourceName += num;
+        } else {
+            resourceName += "9_plus";
+        }
+        int id = getResources().getIdentifier(resourceName, "drawable", this.getPackageName());
+        button.setImageResource(id);
+    }
+
+    private void scanAnimation(int col, int row) {
+        FloatingActionButton button = buttons[row][col];
+        ObjectAnimator animation = ObjectAnimator.ofFloat(button, "alpha", 1f, 0.3f);
+        animation.setRepeatCount(1);
+        animation.setRepeatMode(ObjectAnimator.REVERSE);
+        animation.setDuration(300);
+        animation.start();
+    }
+
+    private void scanRowCol(int col, int row) {
+        for (int i = 0; i < game.getWidth(); i++) {
+            scanAnimation(i, row);
+        }
+        for (int i = 0; i < game.getHeight(); i++) {
+            scanAnimation(col, i);
+        }
+    }
+
+    private void win() {
     }
 }
