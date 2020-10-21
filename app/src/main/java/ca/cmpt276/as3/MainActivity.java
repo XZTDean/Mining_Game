e4898824a7f28a15b3908fe17a916b4f9cd5de45
zetix <zetix@sfu.ca>
2020-10-21 11:14:41 +0800

    Setup button in Menu page

--- app/src/main/java/ca/cmpt276/as3/MainActivity.java
+++ app/src/main/java/ca/cmpt276/as3/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setButton();
+    }
+
+    private void setButton() {
+        Button button = findViewById(R.id.startGame);
+        button.setOnClickListener(v -> {
+            Intent intent = GameActivity.makeIntent(MainActivity.this);
+            startActivity(intent);
+        });
+
+        button = findViewById(R.id.option);
+        button.setOnClickListener(v -> {
+            Intent intent = OptionActivity.makeIntent(MainActivity.this);
+            startActivity(intent);
+        });
+
+        button = findViewById(R.id.helpPage);
+        button.setOnClickListener(v -> {
+            Intent intent = HelpActivity.makeIntent(MainActivity.this);
+            startActivity(intent);
+        });
     }
 }
